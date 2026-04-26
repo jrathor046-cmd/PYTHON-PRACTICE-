d6dd637b4535d367dff1433534a6a3395e0f66ce
Jitendra Singh <jrathor046@gmail.com>
2026-04-26 09:46:06 +0530

    Add day2.py with variables and user input examples

---
+++ basics/day2.py
@@ -0,0 +1,32 @@
+# Day 2 - Variables, Data Types & Input
+
+# Variables
+name = "Jitendra"
+age = 20
+height = 5.7
+is_student = True
+
+# Print variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Data types check
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# User input
+user_name = input("Enter your name: ")
+user_age = int(input("Enter your age: "))
+
+print("Hello my name is", user_name)
+print("I am", user_age, "years old")
+
+# Simple calculation
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum is:", num1 + num2)
